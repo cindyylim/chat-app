eac7cc722a3dcaa4a42207324227d707a8eea889
Cindy <cindysy3@gmail.com>
2025-04-08 12:10:54 -0700

    Add chat layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,15 @@
 import Image from "next/image";
 import PreferencesTab from "@/components/PreferencesTab";
+import { cookies } from "next/headers";
+import ChatLayout from "@/components/chat/ChatLayout";
 
 export default function Home() {
+  const layout = cookies().get("react-resizable-panels:layout");
+  const defaultLayout = layout ? JSON.parse(layout.value) : undefined;
   return <main className="flex h-screen flex-col items-center justify-center p-4 md:px-24 py-32 gap-4">
     <PreferencesTab/>
+    <div className="border rounded-lg max-w-5xl w-full min-h-[85vh text-sm lg:flex">
+      <ChatLayout defaultLayout={defaultLayout}/>
+    </div>
   </main>
 }
