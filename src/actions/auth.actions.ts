f665dedc2f0cf5cfa68ee9c54e6c7718b1ca3ef2
Cindy <cindysy3@gmail.com>
2025-04-20 12:10:54 -0700

    Code formatted

--- src/actions/auth.actions.ts
+++ src/actions/auth.actions.ts
@@ -6,20 +6,22 @@ export async function checkAuthStatus() {
   try {
     const { getUser } = getKindeServerSession();
     const user = await getUser();
-	if (!user) return { success: false };
+    if (!user) return { success: false };
 
     if (user) {
       const userId = `user:${user.id}`;
       const existingUser = await redis.hgetall(userId);
-      
+
       if (!existingUser || Object.keys(existingUser).length === 0) {
         const imgIsNull = user.picture?.includes("gravatar");
         const image = imgIsNull ? "" : user.picture || "";
         const userData = {
           id: user.id || "",
           email: user.email || "",
-          name: `${user.given_name || ""} ${user.family_name || ""}`.trim() || "Anonymous",
-          image: image
+          name:
+            `${user.given_name || ""} ${user.family_name || ""}`.trim() ||
+            "Anonymous",
+          image: image,
         };
         await redis.hset(userId, userData);
       }
--- src/actions/message.actions.ts
+++ src/actions/message.actions.ts
@@ -5,64 +5,79 @@ import { Message } from "@/db/dummy";
 import { pusherServer } from "@/lib/pusher";
 
 type sendMessageActionArgs = {
-    content: string;
-    receiverId: string;
-    messageType: "text" | "image";
-}
-export async function sendMessageAction({content, messageType, receiverId}: sendMessageActionArgs) {
-const {getUser} = getKindeServerSession();
-const user = await getUser();
+  content: string;
+  receiverId: string;
+  messageType: "text" | "image";
+};
+export async function sendMessageAction({
+  content,
+  messageType,
+  receiverId,
+}: sendMessageActionArgs) {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
 
-if (!user) {
-    return {success: false, message: "User not authenticated"};
-}
+  if (!user) {
+    return { success: false, message: "User not authenticated" };
+  }
 
-const senderId = user.id;
-const conversationId = `conversation:${[senderId, receiverId].sort().join(":")}`;
+  const senderId = user.id;
+  const conversationId = `conversation:${[senderId, receiverId]
+    .sort()
+    .join(":")}`;
 
-const conversationExists = await redis.exists(conversationId);
+  const conversationExists = await redis.exists(conversationId);
 
-if (!conversationExists) {
+  if (!conversationExists) {
     await redis.hset(conversationId, {
-        participant1: senderId,
-        participant2: receiverId,
-    })
-    await redis.sadd(`user:${senderId}:conversations`, conversationId)
-    await redis.sadd(`user:${receiverId}:conversations`, conversationId)
+      participant1: senderId,
+      participant2: receiverId,
+    });
+    await redis.sadd(`user:${senderId}:conversations`, conversationId);
+    await redis.sadd(`user:${receiverId}:conversations`, conversationId);
+  }
+  const messageId = `message:${Date.now()}:${Math.random()
+    .toString(36)
+    .substring(2, 9)}`;
+  const timestamp = Date.now();
 
-}
-const messageId = `message:${Date.now()}:${Math.random().toString(36).substring(2, 9)}`;
-const timestamp = Date.now();
-
-await redis.hset(messageId, {
+  await redis.hset(messageId, {
     senderId,
     content,
     timestamp,
     messageType,
-    
-})
+  });
 
-await redis.zadd(`${conversationId}:messages`, {score: timestamp, member: JSON.stringify(messageId)})
+  await redis.zadd(`${conversationId}:messages`, {
+    score: timestamp,
+    member: JSON.stringify(messageId),
+  });
 
-const channelName = `${senderId}__${receiverId}`.split("__").sort().join("__");
-await pusherServer?.trigger(channelName, "newMessage", {
-    message: {senderId, content, timestamp, messageType},
-})
-return {success: true, conversationId, messageId};
-
+  const channelName = `${senderId}__${receiverId}`
+    .split("__")
+    .sort()
+    .join("__");
+  await pusherServer?.trigger(channelName, "newMessage", {
+    message: { senderId, content, timestamp, messageType },
+  });
+  return { success: true, conversationId, messageId };
 }
 
-export async function getMessages(selectedUserId: string, currentUserId: string) {
-    const conversationId = `conversation:${[currentUserId, selectedUserId].sort().join(":")}`;
-    const messageIds = await redis.zrange(`${conversationId}:messages`, 0, -1);
-    if (messageIds.length === 0) {
-        return [];
-    }
-    const pipeline = redis.pipeline();
-    messageIds.forEach((messageId) => {
-        pipeline.hgetall(messageId as string);
-    });
-    const messages = (await pipeline.exec()) as Message[];
-    return messages;
-        
+export async function getMessages(
+  selectedUserId: string,
+  currentUserId: string
+) {
+  const conversationId = `conversation:${[currentUserId, selectedUserId]
+    .sort()
+    .join(":")}`;
+  const messageIds = await redis.zrange(`${conversationId}:messages`, 0, -1);
+  if (messageIds.length === 0) {
+    return [];
+  }
+  const pipeline = redis.pipeline();
+  messageIds.forEach((messageId) => {
+    pipeline.hgetall(messageId as string);
+  });
+  const messages = (await pipeline.exec()) as Message[];
+  return messages;
 }
